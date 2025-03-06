439a9f770d00c953bcdaccc8abeabcd6d2eec7f2
Andrew Keziah <andrew@Andrews-MacBook-Air-300.local>
2025-03-06 14:26:28 -0500

    New nested for loop for year and buffer size

--- cew_extract.R
+++ cew_extract.R
@@ -7,6 +7,8 @@
 library(terra)
 library(landscapemetrics)
 library(tidyverse)
+library(dplyr)
+library(readr)
 
 # load trapping data into the workspace
 data_dir <- "/Volumes/cmjone25/Data/Original/pest-occurrence/corn_earworm/"
@@ -107,3 +109,54 @@ trap_extract <- terra::extract(cdl_2020, trap_buffer)
 percent_corn_1k <- table(trap_extract)[,c("Corn")]/nrow(trap_extract)
 percent_primary_1k <- sum(table(trap_extract)[,c("Corn","Cotton")])/nrow(trap_extract)
 
+# New nested for loop for year & buffer combined
+
+buffer_lengths <- c(200, 400, 600, 800, 1000, 1200)
+
+all_data <- list()
+
+for (year in 2020:2023) {
+  for (buffer in buffer_lengths) {
+    
+    cdl_data <- rast(paste0(cdl_path, year, "_30m_cdls/", year, "_30m_cdls.tif"))
+    cdl_data <- crop(cdl_data, empty_vect)
+    
+    trap_year <- trap_vect[trap_vect$year == year,]
+    
+    trap_buffer <- buffer(trap_year, width = buffer)
+    
+    trap_extract <- terra::extract(cdl_data, trap_buffer)
+    
+    crop_list <- c("Corn", "Cotton", "Peanuts", "Soybeans", "Sorghum", "Sweet Corn", "Tobacco")
+    
+    for (crop in crop_list) {
+      if (crop %in% colnames(table(trap_extract))) {
+        trap_year[[paste0("percent_", tolower(gsub(" ", "", crop)))]] <- table(trap_extract)[,crop] / nrow(trap_extract)
+      } else {
+        trap_year[[paste0("percent_", tolower(gsub(" ", "", crop)))]] <- 0
+      }
+    }
+    
+    trap_year$egg_area <- sum(
+      sapply(crop_list, function(crop) {
+        if (crop %in% colnames(table(trap_extract))) {
+          sum(table(trap_extract)[,crop])
+        } else {
+          0
+        }
+      })
+    )
+    trap_year$year <- year
+    trap_year$buffer <- buffer
+    
+    trap_year <- project(trap_year, "EPSG:4326")
+    trap_data <- cbind(
+      as.data.frame(trap_year),
+      long = crds(trap_year)[,1],
+      lat = crds(trap_year)[,2]
+    )
+    all_data[[paste0(year, "_", buffer)]] <- trap_data
+  }
+}
+combined_data <- do.call(rbind, all_data)
+write_csv(combined_data, file = paste0(output_dir, "/traps_extracted_data.csv"))
